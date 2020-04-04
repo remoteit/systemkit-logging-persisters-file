e5da2f35d00a830bf44f32e0c56e4c682bd567c5
codemodify <codemodify@member.fsf.org>
2020-04-04 05:27:13 -0700

    adding more helpers

--- helpers.go
+++ helpers.go
@@ -12,13 +12,20 @@ func fileOrFolderExists(name string) bool {
 	return !os.IsNotExist(err)
 }
 
+type emptyWritter struct{}
+
+func (thisRef emptyWritter) Write(p []byte) (n int, err error) {
+	return 0, nil
+}
+
 // NewFileLoggerDefaultName -
 func NewFileLoggerDefaultName() logging.Logger {
 	return NewFileLogger(fmt.Sprintf("%s.log", os.Args[0]), &emptyWritter{})
 }
 
-type emptyWritter struct{}
-
-func (thisRef emptyWritter) Write(p []byte) (n int, err error) {
-	return 0, nil
+// NewFileLoggerDefaultNameEasy -
+func NewFileLoggerDefaultNameEasy() logging.LoggerImplementation {
+	return logging.NewDefaultLoggerImplementation(
+		NewFileLoggerDefaultName(),
+	)
 }
